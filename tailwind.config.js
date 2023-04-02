0755b103315021ec287889461adfd3009edd3b2c
Erick <batista.erick@outlook.com>
2023-04-02 17:41:32 -0300

    feature: creating the authentication page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/app/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
+  content: [
+    './src/app/*.{ts,tsx}',
+    './src/app/**/*.{ts,tsx}',
+    './src/components/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
